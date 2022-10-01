beaac8850ba75e7717391edafe5abf42a09b7c01
felpsdev <felaxx.developer@gmail.com>
2022-10-01 15:29:34 -0300

    fix errors

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -5,7 +5,7 @@ import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
-        <CoreProvider colorScheme="light">
+        <CoreProvider colorScheme="dark">
             <App />
         </CoreProvider>
     </React.StrictMode>,
